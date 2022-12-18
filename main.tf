a190197db6b23ced684fe57139485dd10650c786
careem111 <krm.mrt@gmail.com>
2022-12-18 15:23:12 +0530

    working first commit

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 
 
 module "webserver_cluster" {
-    source = "github.com/careem111/terraform-module?ref=v0.0.1"
+    source = "github.com/careem111/terraform-module?ref=v0.0.3"
   
     cluster_name = "webserver"
     instance_type = "t2.micro"
     keyname = "krm-virginia"
     min_size = 2
     max_size = 2
+    html_body = "This is my first website"
 
 }
+
+
+#github.com/careem111/terraform-module?ref=v0.0.2
