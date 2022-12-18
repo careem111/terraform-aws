003effc2d86f2714fa228bf89c20bfb375e05824
careem111 <krm.mrt@gmail.com>
2022-12-18 14:14:42 +0530

    module modified

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 
 
 module "webserver_cluster" {
-    source = "https://github.com/careem111/terraform-module?ref=v0.0.1"
+    source = "github.com/careem111/terraform-module?ref=v0.0.1"
   
     cluster_name = "webserver"
-    db_remote_state_bucket = "terraform-backend-module-krm"
-    db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
     instance_type = "t2.micro"
     keyname = "krm-virginia"
     min_size = 2
